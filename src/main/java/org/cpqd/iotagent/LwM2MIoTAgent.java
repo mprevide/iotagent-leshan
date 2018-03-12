c2b5be07efa3083fcc9c21bd8194ec3234f42348
Juliano Siloto Assine <jsiloto@cpqd.com.br>
2018-03-12 15:14:24 -0300

    Add Kafka consumer

--- src/main/java/org/cpqd/iotagent/LwM2MIoTAgent.java
+++ src/main/java/org/cpqd/iotagent/LwM2MIoTAgent.java
@@ -5,9 +5,16 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.cpqd.iotagent.LwM2mAgent;
+import org.cpqd.iotagent.KafkaConsumerLoop;
+
+import org.apache.kafka.clients.consumer.*;
+import java.util.Properties;
 
 import static spark.Spark.get;
 
@@ -24,6 +31,12 @@ public class LwM2MIoTAgent {
 
         get("/update", (req, res) -> agent.update(req.body()));
         get("/actuate", (req, res) -> agent.actuate(req.body()));
+
+
+        KafkaConsumerLoop consumerLoop = new KafkaConsumerLoop(0, "dojot", Arrays.asList("test"));
+        consumerLoop.run();
+
+
     }
 
 }
