7355e4300e3787dfa6ed7f3a5772bfc8cf8a1dbb
Juliano Siloto Assine <julianosiloto@gmail.com>
2018-04-22 20:47:32 -0300

    Implement on_remove operation

--- src/main/java/org/cpqd/iotagent/LwM2mAgent.java
+++ src/main/java/org/cpqd/iotagent/LwM2mAgent.java
@@ -176,6 +176,10 @@ public class LwM2mAgent implements Runnable {
 
     private Integer on_remove(JSONObject message) {
         mLogger.debug("on_remove: " + message.toString());
+        JsonObject o = new JsonParser().parse(message.toString()).getAsJsonObject();
+        String deviceId = o.get("id").getAsString();
+        Registration registration = deviceManager.getDeviceRegistration(deviceId);
+        deviceManager.DeregisterDevice(registration.getId());
         return 0;
     }
 
