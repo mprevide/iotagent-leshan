a74e84c798b5c693bc67eff6406a21db310ec6ce
Raul Negreiros <rauln@cpqd.com.br>
2018-08-13 08:30:54 -0300

    Adds back the file server's copyright

--- src/main/java/org/cpqd/iotagent/SimpleFileServer.java
+++ src/main/java/org/cpqd/iotagent/SimpleFileServer.java
@@ -1,6 +1,22 @@
-/**
- * @description A simple file server based on eclipse leshan example "SimpleFileServer". It supports DTLS.
- */
+/*******************************************************************************
+ * Copyright (c) 2017 Bosch Software Innovations GmbH and others.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.html.
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - initial creation
+ *                                      derived from HelloWorldServer example
+ *    Bosch Software Innovations GmbH - migrate to SLF4J
+ *    Dojot developers team           - removed the main method and transformed
+ *                                      it into a class
+ ******************************************************************************/
 
 package org.cpqd.iotagent;
 
