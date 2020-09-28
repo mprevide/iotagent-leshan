46e988b9fedf685ce8b5fec60e59b0e01a3b53e9
canattofilipe <canatto.filipe@gmail.com>
2020-09-28 11:37:02 -0300

    Revert "Changes all content request to use TLV"
    This reverts commit 2e9d8d15a0e31345172531d7be22446e1d48e4fe.

--- src/main/java/org/cpqd/iotagent/LwM2mHandler.java
+++ src/main/java/org/cpqd/iotagent/LwM2mHandler.java
@@ -10,7 +10,6 @@ In case of failure any request should fail silently
 
 import org.eclipse.leshan.core.request.ExecuteRequest;
 import org.eclipse.leshan.core.request.ObserveRequest;
-import org.eclipse.leshan.core.request.ContentFormat;
 import org.apache.log4j.Logger;
 import org.eclipse.leshan.core.request.WriteRequest;
 import org.eclipse.leshan.core.request.ReadRequest;
@@ -42,7 +41,7 @@ public class LwM2mHandler {
         try {
             Integer pathArray[] = DeviceAttribute.getIdsfromPath(path);
             ObserveResponse response = server.send(registration,
-                new ObserveRequest(ContentFormat.fromCode(ContentFormat.TLV_CODE), pathArray[0], pathArray[1], pathArray[2]), readTimout);
+                new ObserveRequest(pathArray[0], pathArray[1], pathArray[2]), readTimout);
             if (response == null) {
                 this.mLogger.error("observe request timed out");
                 return null;
@@ -90,7 +89,7 @@ public class LwM2mHandler {
         try {
             Integer pathArray[] = DeviceAttribute.getIdsfromPath(path);
             ReadResponse response = server.send(registration,
-                new ReadRequest(ContentFormat.fromCode(ContentFormat.TLV_CODE), pathArray[0], pathArray[1], pathArray[2]), readTimout);
+                new ReadRequest(pathArray[0], pathArray[1], pathArray[2]), readTimout);
             if (response == null) {
                 this.mLogger.error("read request timed out");
                 return null;
@@ -118,7 +117,7 @@ public class LwM2mHandler {
         try {
             Integer pathArray[] = DeviceAttribute.getIdsfromPath(path);
             ReadResponse response = server.send(registration,
-                new ReadRequest(ContentFormat.fromCode(ContentFormat.TLV_CODE), pathArray[0], pathArray[1], pathArray[2]), readTimout);
+                new ReadRequest(pathArray[0], pathArray[1], pathArray[2]), readTimout);
             if (response == null) {
                 this.mLogger.error("read request timed out");
                 return null;
